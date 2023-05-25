0a7eb4b17cf57314e24f7bcc6e3fed6d39a97791
MahtoSujeet <sujeetmehta6@gmail.com>
2023-05-26 01:46:23 +0530

    added few cmp settings & made background transparent

--- nvim/lua/plugins/githubTheme.lua
+++ nvim/lua/plugins/githubTheme.lua
@@ -1,11 +1,6 @@
 return {
   'projekt0n/github-nvim-theme',
-  tag = 'v0.0.7',
-  --   config = function()
-  --     vim.cmd.colorscheme("github_dark_default")
-  --
-  -- -- To make bg transparent
-  --     vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
-  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
-  --   end
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
 }
+
--- nvim/lua/plugins/zerolsp.lua
+++ nvim/lua/plugins/zerolsp.lua
@@ -28,5 +28,24 @@ return {
     end)
 
     lsp.setup()
+
+    local cmp = require('cmp')
+    local cmp_action = require('lsp-zero').cmp_action()
+
+    cmp.setup({
+      mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+      },
+      preselect = 'item',
+      completion = {
+        completeopt = 'menu,menuone,noinsert'
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    })
   end
 }
