ec158417dcd7a575db31504557ee6444b0ea8b87
MahtoSujeet <sujeetmehta6@gmail.com>
2023-11-30 22:48:56 +0530

    General: tweaks here and there

--- .config/nvim/lua/plugins/dashboard-nvim.lua
+++ .config/nvim/lua/plugins/dashboard-nvim.lua
@@ -1,9 +1,13 @@
 return {
-  'glepnir/dashboard-nvim',
+  'nvimdev/dashboard-nvim',
   event = 'VimEnter',
   config = function()
     require('dashboard').setup {
-      -- config
+      config = {
+        week_header = {
+          enable = true,
+        },
+      }
     }
   end,
   dependencies = { { 'nvim-tree/nvim-web-devicons' } }
