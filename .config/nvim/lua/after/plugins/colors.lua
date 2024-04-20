a15b48851c17ca7800566248bf75c4ef0b86ce8e
MahtoSujeet <sujeetmehta6@gmail.com>
2024-04-20 10:02:26 +0530

    nvim : added foldings

--- .config/nvim/lua/after/plugins/colors.lua
+++ .config/nvim/lua/after/plugins/colors.lua
@@ -1,14 +1,13 @@
-function ColorMyPencils(color)
-  color = color or "gruvbox"
-  local transparent = false
-
-  vim.cmd.colorscheme(color)
-
-  -- To make bg transparent
-  -- if (transparent) then
-  --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
-  --   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
-  -- end
-end
-
-ColorMyPencils()
+vim.cmd.colorscheme("gruvbox")
+-- function ColorMyPencils(color)
+--   color = color or "gruvbox"
+--   local transparent = false
+--
+--   vim.cmd.colorscheme(color)
+--
+--   -- To make bg transparent
+--   -- if (transparent) then
+--   --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
+--   --   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
+--   -- end
+-- end
