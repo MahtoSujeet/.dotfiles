e78f6f8cd137b5ec5f20a84bdbc77cce68234c13
MahtoSujeet <sujeetmehta6@gmail.com>
2024-04-20 10:02:26 +0530

    nvim : added foldings

--- .config/nvim/lua/myconfig/set.lua
+++ .config/nvim/lua/myconfig/set.lua
@@ -18,12 +18,15 @@ vim.opt.scrolloff = 3
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.g.mapleader = " "
+-- Code folding
+-- By default all the foldings are open
+-- close all = zM
+-- toggle one = za
+-- open all = zR
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
--- Better Netrw
--- vim.g.netrw_banner = 0    -- Hide banner
--- vim.g.netrw_winsize = 75
--- vim.g.netrw_altv = 2      -- Open with right splitting
--- vim.g.netrw_liststyle = 3 -- Tree-style view
--- -- use .gitignore
--- vim.g.netrw_list_hide = (vim.fn["netrw_gitignore#Hide"]()) .. [[,\(^\|\s\s\)\zs\.\S\+]]
+vim.opt.foldlevel = 999
+vim.opt.foldnestmax = 4
+
+vim.g.mapleader = " "
