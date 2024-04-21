84b829ae67c5c8f86cab7f25bd43512f782027da
MahtoSujeet <sujeetmehta6@gmail.com>
2024-04-21 13:14:02 +0530

    nvim : fixed lazy loading of diffviwe

--- .config/nvim/lua/plugins/diffview-nvim.lua
+++ .config/nvim/lua/plugins/diffview-nvim.lua
@@ -1,11 +1,13 @@
 return {
   "sindrets/diffview.nvim",
   lazy = true,
+  cmd = { "DiffviewOpen" },
+  keys = { "<leader>gd" },
   config = function()
-    vim.keymap.set("n", "<leader>g", ":DiffviewOpen<cr>")
+    vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<cr>")
 
     -- For dotfiles
-    vim.env.GIT_WORK_TREE = vim.fn.expand("~")
-    vim.env.GIT_DIR = vim.fn.expand("~/.dotfiles")
+    -- vim.env.GIT_WORK_TREE = vim.fn.expand("~")
+    -- vim.env.GIT_DIR = vim.fn.expand("~/.dotfiles")
   end
 }
