5076882c0b2500c548107938051cc1a68e9b8356
MahtoSujeet <sujeetmehta6@gmail.com>
2024-07-10 11:18:44 +0530

    nvim : enabled transparent bg

--- .config/nvim/lua/after/plugins/colors.lua
+++ .config/nvim/lua/after/plugins/colors.lua
@@ -1,16 +1,18 @@
-vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("gruvbox")
 -- vim.cmd.colorscheme("tokyonight")
 -- vim.cmd.colorscheme("rose-pine")
 -- vim.cmd.colorscheme("catppuccin")
--- function ColorMyPencils(color)
---   color = color or "gruvbox"
---   local transparent = false
---
---   vim.cmd.colorscheme(color)
---
---   -- To make bg transparent
---   -- if (transparent) then
---   --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
---   --   -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
---   -- end
--- end
+function ColorMyPencils(color)
+  color = color or "gruvbox"
+  local transparent = true
+
+  vim.cmd.colorscheme(color)
+  --
+  -- To make bg transparent
+  if (transparent) then
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
+  end
+end
+
+ColorMyPencils()
