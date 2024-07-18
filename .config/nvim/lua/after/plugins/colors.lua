8047d1965f6b7d2afce6845128a5a56f210ecdfe
MahtoSujeet <sujeetmehta6@gmail.com>
2024-07-18 10:41:33 +0530

    nvim : made telescope bg opaque

--- .config/nvim/lua/after/plugins/colors.lua
+++ .config/nvim/lua/after/plugins/colors.lua
@@ -7,11 +7,24 @@ function ColorMyPencils(color)
   local transparent = true
 
   vim.cmd.colorscheme(color)
+  local bg_color = vim.api.nvim_get_hl_by_name("Normal", true).background
+  bg_color = string.format("#%06x", bg_color)
+  print(bg_color)
+
   --
   -- To make bg transparent
   if (transparent) then
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" }) -- 0 means global
-    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- 0 means global
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })            -- 0 means global
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })       -- 0 means global
+    vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = bg_color }) -- 0 means global
+
+
+    -- not working
+    vim.api.nvim_set_hl(0, "ToggleTermNormal", { bg = bg_color })       -- 0 means global
+    vim.api.nvim_set_hl(0, "ToggleTermNormalFloat", { bg = bg_color })  -- 0 means global
+    vim.api.nvim_set_hl(0, "ToggleTerm1NormalFloat", { bg = bg_color }) -- 0 means global
+    vim.api.nvim_set_hl(0, "ToggleTermFloatBorder", { bg = bg_color })  -- 0 means global
+    vim.api.nvim_set_hl(0, "TerminalNormal", { bg = bg_color })         -- 0 means global
   end
 end
 
