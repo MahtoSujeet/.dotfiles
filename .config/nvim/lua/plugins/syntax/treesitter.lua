09cd3d590dc63b066bd63cfa2b6322be64725224
MahtoSujeet <sujeetmehta6@gmail.com>
2024-12-25 13:55:37 +0530

    nvim : refactor

--- .config/nvim/lua/plugins/syntax/treesitter.lua
+++ .config/nvim/lua/plugins/syntax/treesitter.lua
@@ -25,45 +25,45 @@ return {
         enable = true,
       },
 
-      incremental_selection = {
-        keymaps = {
-          init_selection = "<leader>ss",
-          node_incremental = "<leader>si",
-          scope_incremental = "<leader>sc",
-          node_decremental = "<leader>sd"
-        },
-      },
+      -- incremental_selection = {
+      --   keymaps = {
+      --     init_selection = "<leader>ss",
+      --     node_incremental = "<leader>si",
+      --     scope_incremental = "<leader>sc",
+      --     node_decremental = "<leader>sd"
+      --   },
+      -- },
 
       textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
+        -- select = {
+        --   enable = true,
+        --   lookahead = true,
 
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-          },
+        --   -- keymaps = {
+        --   --   -- You can use the capture groups defined in textobjects.scm
+        --   --   ["af"] = "@function.outer",
+        --   --   ["if"] = "@function.inner",
+        --   --   ["ac"] = "@class.outer",
+        --   --   ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        --   -- },
 
-          -- default is "v"
-          -- selection_modes = {
-          --   ['@parameter.outer'] = 'v', -- charwise
-          --   ['@function.outer'] = 'V', -- linewise
-          --   ['@class.outer'] = '<c-v>', -- blockwise
-          -- },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true or false
-          include_surrounding_whitespace = true,
-        },
+        --   -- default is "v"
+        --   -- selection_modes = {
+        --   --   ['@parameter.outer'] = 'v', -- charwise
+        --   --   ['@function.outer'] = 'V', -- linewise
+        --   --   ['@class.outer'] = '<c-v>', -- blockwise
+        --   -- },
+        --   -- If you set this to `true` (default is `false`) then any textobject is
+        --   -- extended to include preceding or succeeding whitespace. Succeeding
+        --   -- whitespace has priority in order to act similarly to eg the built-in
+        --   -- `ap`.
+        --   --
+        --   -- Can also be a function which gets passed a table with the keys
+        --   -- * query_string: eg '@function.inner'
+        --   -- * selection_mode: eg 'v'
+        --   -- and should return true or false
+        --   include_surrounding_whitespace = true,
+        -- },
 
         move = {
           enable = true,
