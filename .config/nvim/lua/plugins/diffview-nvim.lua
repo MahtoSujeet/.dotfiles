5171f271e05a707f9fa189b5bbebfa9b16a26e9f
MahtoSujeet <sujeetmehta6@gmail.com>
2024-12-25 13:54:37 +0530

    nvim : added desc for keybinds
    the keybinds can be seen with <leader>fk

--- .config/nvim/lua/plugins/diffview-nvim.lua
+++ .config/nvim/lua/plugins/diffview-nvim.lua
@@ -1,11 +1,14 @@
 return {
   "sindrets/diffview.nvim",
   lazy = true,
-  cmd = { "DiffviewOpen" },
-  keys = { "<leader>gd" },
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Git diff view" },
+    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Close Git diff view" },
+    { "<leader>gt", "<cmd>DiffviewToggleFiles<cr>", desc = "Toggle diff file panel" },
+    { "<leader>gf", "<cmd>DiffviewFocusFiles<cr>", desc = "Focus diff file panel" },
+  },
   config = function()
-    vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<cr>")
-
     -- For dotfiles
     -- vim.env.GIT_WORK_TREE = vim.fn.expand("~")
     -- vim.env.GIT_DIR = vim.fn.expand("~/.dotfiles")
