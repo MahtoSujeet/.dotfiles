97395bdda588992d56ee87f388bc660bf5919c65
MahtoSujeet <sujeetmehta6@gmail.com>
2024-12-31 20:33:16 +0530

    nvim : added CP keybinds

--- .config/nvim/lua/plugins/coding/competitest.lua
+++ .config/nvim/lua/plugins/coding/competitest.lua
@@ -2,13 +2,15 @@ return {
   'xeluxee/competitest.nvim',
   dependencies = 'MunifTanjim/nui.nvim',
   config = function()
-    require('competitest').setup()
+    require('competitest').setup({
+      testcases_use_single_file = true,
+    })
 
     local set = vim.keymap.set
 
     set('n', '<leader>cr', "<cmd>CompetiTest run<CR>")
-    set('n', '<leader>ct', "<cmd>CompeTiTest recieve testcases<CR>")
-    set('n', '<leader>cp', "<cmd>CompeTiTest recieve problem<CR>")
-    set('n', '<leader>cc', "<cmd>CompeTiTest recieve contest<CR>")
+    set('n', '<leader>ct', "<cmd>CompetiTest receive testcases<CR>")
+    set('n', '<leader>cp', "<cmd>CompetiTest receive problem<CR>")
+    set('n', '<leader>cc', "<cmd>CompetiTest receive contest<CR>")
   end
 }
