714f0c9c6a4be25b035080faab8b8b06aa720378
MahtoSujeet <sujeetmehta6@gmail.com>
2024-12-31 20:33:34 +0530

    nvim : improve nvimtree UI

--- .config/nvim/lua/plugins/ui/nvim-tree.lua
+++ .config/nvim/lua/plugins/ui/nvim-tree.lua
@@ -6,7 +6,38 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      filters = { dotfiles = false },
+      disable_netrw = true,
+      hijack_cursor = true,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      view = {
+        width = 30,
+        preserve_window_proportions = true,
+      },
+      renderer = {
+        root_folder_label = false,
+        highlight_git = true,
+        indent_markers = { enable = true },
+        icons = {
+          glyphs = {
+            default = "󰈚",
+            folder = {
+              default = "",
+              empty = "",
+              empty_open = "",
+              open = "",
+              symlink = "",
+            },
+            git = { unmerged = "" },
+          },
+        },
+      },
+    }
     vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
   end,
 }
